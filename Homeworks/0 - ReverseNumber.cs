01971e04e52c6058970b3dab8ed705c694b5c09d
MilkyW <minniewu@qq.com>
2019-07-11 16:02:58 +0800

    Create 0 - ReverseNumber.cs

---
+++ Homeworks/0 - ReverseNumber.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace wxr
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = 123;
+            int temp = ReverseNumber(n); //321
+            Console.WriteLine(temp);
+
+            n = -123;
+            temp = ReverseNumber(n); //-321
+            Console.WriteLine(temp);
+
+            n = 1023456789;
+            temp = ReverseNumber(n); //overflow
+            Console.WriteLine(temp);
+        }
+
+        static int ReverseNumber(int score)
+        {
+            bool isNegative = false;
+            if (score < 0)
+            {
+                isNegative = true;
+                score = -score;
+            }
+
+            string str = score.ToString().Reverse();
+            string maxInt = int.MaxValue.ToString();
+
+            if ((str.Length > maxInt.Length)
+                || (str.Length == maxInt.Length && string.Compare(str, maxInt) > 0))
+            {
+                throw OverflowException();
+            }
+
+            score = int.Parse(str);
+
+            if (isNegative)
+            {
+                score = -score;
+            }
+
+            return score;
+        }
+
+        private static Exception OverflowException()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    static class ExtendedMethods
+    {
+        public static string Reverse(this string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
